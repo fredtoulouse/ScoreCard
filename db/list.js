62b1ba817e02c512958b23c6805e5179f258e995
fredtoulouse <player.golfscorecard@gmail.com>
2013-10-30 21:24:47 +0100

    Firefox BETA1

--- db/list.js
+++ db/list.js
@@ -2,11 +2,12 @@
 var listGolf;
 
 listGolf = [
-["Saint Gabriel", [43.650853,1.535255], "France", "Midi-Pyrénées"],
-["Fleurance", [43.815029,0.640265], "France", "Midi-Pyrénées"],
-["Fleurance Alt", [43.815029,0.640265], "France", "Midi-Pyrénées"],
-["La Ramee Compact", [43.576802, 1.36193316], "France", "Midi-Pyrénées"],
-["Las Martines", [43.63973, 1.107873], "France", "Midi-Pyrénées"],
-["Es Tolosa", [43.63973, 1.107873], "France", "Midi-Pyrénées"],
+["Saint Gabriel", [43.650853,1.535255], "France", "Midi-Pyrénées", 9],
+["Fleurance", [43.815029,0.640265], "France", "Midi-Pyrénées", 9],
+["Fleurance Alt", [43.815029,0.640265], "France", "Midi-Pyrénées", 9],
+["La Ramee Compact", [43.576802, 1.36193316], "France", "Midi-Pyrénées", 9],
+["Las Martines", [43.63973, 1.107873], "France", "Midi-Pyrénées", 9],
+["Es Tolosa", [43.63973, 1.107873], "France", "Midi-Pyrénées", 9],
+/*["Compact Saint Gabriel", [43.648702,1.537211], "France", "Midi-Pyrénées", 6],*/
 ];
 	
